e58fd5b016260e24452f9bf5a46a29e26724f73e
menthy-wu <wumengxuan0728@gmail.com>
2023-10-01 12:44:33 -0700

    setup

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Input = ({
+  placehoder,
+  button,
+  onClick,
+  dropDown,
+  placeholder,
+  value,
+  setValue,
+  clear,
+}) => {
+  return <div>Input</div>;
+};
+
+export default Input;
