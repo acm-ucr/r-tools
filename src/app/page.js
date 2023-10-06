05b9a1cd945f6fd8e5af865bc1034d4298d585c9
menthy-wu <wumengxuan0728@gmail.com>
2023-10-06 00:42:16 -0700

    reset page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-import BarChart from "@/components/charts/BarChart";
 const Home = () => {
-  return (
-    <>
-      <BarChart data={[12, 5, 6, 6, 9, 3]} />
-    </>
-  );
+  return <>Hello world</>;
 };
 
 export default Home;
