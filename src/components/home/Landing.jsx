bc909271356b4c98a463cca5dbe538fc200f5a2b
Isaac Castro <icast065@ucr.edu>
2023-10-07 22:21:20 -0700

    Created landing component

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Logo from "../../Assets/RToolsLogo.png"
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+  
+  <div className="flex flex-col items-center">
+
+    <Image src={Logo} height={250} width={250}/>
+    <p className="text-center py-3 max-w-4xl text-blue-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut qu consectetur adipiscing elit, sed do eiusmod tempor incididunt ut qu</p>
+
+
+  </div>);
 };
 
 export default Landing;
