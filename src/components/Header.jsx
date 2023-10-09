037832d435dfd99e56381d570aa720c83e42912a
Isaac Castro <icast065@ucr.edu>
2023-10-08 22:15:13 -0700

    Made the div self closing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = ({ text }) => {
   return (
     <div>
       <p className="text-white text-4xl mb-0">{text}</p>
-      <div className="bg-green-300 rounded h-1 w-20"/>
+      <div className="bg-green-300 rounded h-1 w-20" />
     </div>
   );
 };
