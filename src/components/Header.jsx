58270dbce3290e1e5190faa84215c1b19a70c491
Isaac Castro <icast065@ucr.edu>
2023-10-08 22:07:26 -0700

    Changed h1 tag to p tag in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ text }) => {
   return (
     <div>
-      <h1 className="text-white text-4xl mb-0">{text}</h1>
+      <p className="text-white text-4xl mb-0 tag">{text}</p>
       <div className="bg-green-300 rounded h-1 w-20"></div>
     </div>
   );
