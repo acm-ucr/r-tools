c6980bf48dfe45b6cdbdd9b7172b76fd2ad61724
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-10-13 19:42:07 -0700

    Created BoolToolBar

--- src/components/math/BoolToolBar.jsx
+++ src/components/math/BoolToolBar.jsx
@@ -1,7 +1,61 @@
+"use client"; // This is a client component
+
 import React from "react";
+import { useState } from "react";
 
 const BoolToolBar = () => {
-  return <div>BoolToolBar</div>;
+  const [and, setAnd] = useState("");
+  const [or, setOr] = useState("");
+  const [not, setNot] = useState("");
+
+  function isAnd(input) {
+    setAnd(input.target.value);
+  }
+
+  function isOr(input) {
+    setOr(input.target.value);
+  }
+
+  function isNot(input) {
+    setNot(input.target.value);
+  }
+
+  return (
+    <div className=" flex-col grid max-w-xs grid-cols-3">
+      <div className="flex-col grid w-20 grid-cols-2">
+        <p>and:</p>
+        <div className="bg-rtools-blue-300 w-12 flex justify-center items-center h-8 rounded">
+          <input
+            className="bg-transparent w-8 h-8 rounded outline-none"
+            value={and}
+            onChange={isAnd}
+          />
+        </div>
+      </div>
+      <div className="flex-col grid max-w-xs gap-x-20 grid-cols-2">
+        <div className="flex-col grid w-12 grid-cols-2">
+          <p>or:</p>
+          <div className="bg-rtools-blue-300 flex justify-center items-center w-12 h-8 rounded">
+            <input
+              className="bg-transparent w-8 h-8 rounded outline-none"
+              value={or}
+              onChange={isOr}
+            />
+          </div>
+        </div>
+        <div className="flex-col grid w-20 grid-cols-2">
+          <p>not:</p>
+          <div className="bg-rtools-blue-300 flex justify-center items-center w-12 h-8 rounded">
+            <input
+              className="bg-transparent w-8 h-8 rounded outline-none"
+              value={not}
+              onChange={isNot}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoolToolBar;
