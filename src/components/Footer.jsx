dbe945e7ff43aaf33b82e06baea3c8ab1a761f3b
daniel-u-c <dcast162@ucr.edu>
2023-10-16 15:20:58 -0700

    Footer zoom in fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,16 @@ import Logo from "../Assets/RToolsLogo.svg";
 
 const Footer = () => {
   return (
-    <div className="h-28 mx-8 mb-2 text-white flex justify-between items-end">
-      <div className="h-full flex items-center shrink-0">
+    <div className="h-28 mx-8 text-white flex justify-between items-end gap-24 border-2 border-red-500">
+      <div className="h-full flex items-center shrink-0 border-2 border-red-500">
         <Image src={Logo} />
       </div>
-      <div className="text-sm">© Copyright acm@ucr All rights reserved</div>
-      <div className="text-base">contact.acmucr@gmail.com</div>
+      <div className="text-sm border-2 border-red-500 whitespace-nowrap">
+        © Copyright acm@ucr All rights reserved
+      </div>
+      <div className="text-base border-2 border-red-500">
+        contact.acmucr@gmail.com
+      </div>
     </div>
   );
 };
