9210ca2d7667e0afb721bc58d7bb3abf19658bf9
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-10-16 10:25:47 -0700

    Made states into props and made the inputs into a map

--- src/components/math/BoolToolBar.jsx
+++ src/components/math/BoolToolBar.jsx
@@ -1,57 +1,27 @@
 import React from "react";
-import { useState } from "react";
 
-const BoolToolBar = () => {
-  const [and, setAnd] = useState("");
-  const [or, setOr] = useState("");
-  const [not, setNot] = useState("");
-
-  function isAnd(input) {
-    setAnd(input.target.value);
-  }
-
-  function isOr(input) {
-    setOr(input.target.value);
-  }
-
-  function isNot(input) {
-    setNot(input.target.value);
-  }
+const BoolToolBar = ({ and, or, not }) => {
+  const symbols = { and: "&", or: "|", not: "~" };
 
   return (
     <div className=" flex-col grid max-w-xs grid-cols-3">
-      <div className="flex-col grid w-20 grid-cols-2">
-        <p>and:</p>
-        <div className="bg-rtools-blue-300 w-12 h-6 flex justify-center items-center h-8 rounded">
-          <input
-            className="bg-transparent w-8 h-6 rounded outline-none"
-            value={and}
-            onChange={isAnd}
-          />
-        </div>
-      </div>
-      <div className="flex-col grid max-w-xs gap-x-20 grid-cols-2">
-        <div className="flex-col grid w-12 grid-cols-2">
-          <p>or:</p>
-          <div className="bg-rtools-blue-300 flex justify-center items-center w-12 h-6 rounded">
+      {Object.entries(symbols).map(([symbol, value], index) => (
+        <div className="flex-col grid w-20 grid-cols-2" key={index}>
+          <p>{symbol}:</p>
+          <div
+            key={index}
+            className="bg-rtools-blue-300 flex justify-center items-center w-12 h-6 rounded"
+          >
             <input
               className="bg-transparent w-8 h-6 rounded outline-none"
-              value={or}
-              onChange={isOr}
+              value={value}
+              onChange={(e) => {
+                setSymbols({ ...symbols, [symbol]: e.target.value });
+              }}
             />
           </div>
         </div>
-        <div className="flex-col grid w-20 grid-cols-2">
-          <p>not:</p>
-          <div className="bg-rtools-blue-300 flex justify-center items-center w-12 h-6 rounded">
-            <input
-              className="bg-transparent w-8 h-6 rounded outline-none"
-              value={not}
-              onChange={isNot}
-            />
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
