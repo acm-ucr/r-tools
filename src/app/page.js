d3615ecb514e88fa0a6d24d4d93dc71dbb3ecff5
menthy-wu <wumengxuan0728@gmail.com>
2023-10-19 15:51:59 -0700

    fix the color

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,16 @@
-import BarChart from "@/components/charts/BarChart";
+import CodeView from "@/components/CodeView";
 
+const code = {
+  "C++":
+    "void selectionSort(int arr[], int n) {\n    for (int i = 0; i < n - 1; i++) {\n        // Find the minimum element in the unsorted part of the array\n        int minIndex = i;\n        for (int j = i + 1; j < n; j++) {\n            if (arr[j] < arr[minIndex]) {\n                minIndex = j;\n            }\n        }\n        // Swap the found minimum element with the first element\n        int temp = arr[i];\n        arr[i] = arr[minIndex];\n        arr[minIndex] = temp;\n    }\n}",
+  Java: "public static void selectionSort(int[] arr) {\n        int n = arr.length;\n\n        for (int i = 0; i < n - 1; i++) {\n            int minIndex = i;\n\n            // Find the index of the minimum element in the unsorted part of the array\n            for (int j = i + 1; j < n; j++) {\n                if (arr[j] < arr[minIndex]) {\n                    minIndex = j;\n                }\n            }\n\n            // Swap the minimum element with the first element of the unsorted part\n            int temp = arr[i];\n            arr[i] = arr[minIndex];\n            arr[minIndex] = temp;\n        }\n    }",
+  Python:
+    "def selection_sort(arr):\n    n = len(arr)\n    for i in range(n):\n        min_index = i\n        # Find the index of the minimum element in the unsorted part of the array\n        for j in range(i + 1, n):\n            if arr[j] < arr[min_index]:\n                min_index = j\n        # Swap the minimum element with the first element of the unsorted part\n        arr[i], arr[min_index] = arr[min_index], arr[i]",
+};
 const Home = () => {
   return (
     <>
-      <BarChart
-        width={600}
-        height={300}
-        data={[
-          { number: 4, highlight: false },
-          { number: 10, highlight: false },
-          { number: 5, highlight: false },
-          { number: 5, highlight: false },
-          { number: 6, highlight: false },
-          { number: 7, highlight: false },
-          { number: 9, highlight: false },
-        ]}
-      />
+      <CodeView codes={code} />
     </>
   );
 };
--- src/components/CodeView.jsx
+++ src/components/CodeView.jsx
@@ -1,8 +1,27 @@
 "use client";
 
 import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import { LANGUAGES } from "@/data/language";
 
-const CodeView = () => {
+const setEditorTheme = (monaco) => {
+  monaco.editor.defineTheme("my-theme", {
+    base: "vs-dark",
+    inherit: true,
+    rules: [
+      { token: "comment", foreground: "B3EFB9" },
+      { token: "keyword", foreground: "12D0D5" },
+      { token: "number", foreground: "FF8AB4" },
+      { token: "string", foreground: "FFC700" },
+      { token: "tag", foreground: "BE8AFF" },
+    ],
+    colors: {
+      "editor.foreground": "#FFFFFF",
+    },
+  });
+};
+const CodeView = ({ codes }) => {
+  const [select, setSelect] = useState(Object.keys(codes)[0]);
   return (
     <div className="rounded-lg bg-rtools-blue-300">
       <div className="flex justify-between p-2">
@@ -12,35 +31,34 @@ const CodeView = () => {
           <div className="p-1.5 rounded-full bg-rtools-teal-200" />
         </div>
         <div className="flex gap-2">
-          <div className="rounded-full px-4 text-center bg-rtools-blue-200">
-            <p className="text-center m-0 p-0">C++</p>
-          </div>
-          <div className="rounded-full px-3">
-            <p className="m-0 p-0">Python</p>
-          </div>
-          <div className="rounded-full px-3">
-            <p className="m-0 p-0">Javascript</p>
-          </div>
-          <div className="rounded-full px-3">
-            <p className="m-0 p-0">Java</p>
-          </div>
-          <div className="rounded-full px-3">
-            <p className="m-0 p-0">C#</p>
-          </div>
+          {Object.keys(codes).map((language, index) => (
+            <div
+              onClick={() => setSelect(language)}
+              key={index}
+              className={`rounded-full px-4 text-center hover:cursor-pointer ${
+                select === language && "bg-rtools-blue-200"
+              }`}
+            >
+              <p className="text-center m-0 p-0">{language}</p>
+            </div>
+          ))}
         </div>
       </div>
       <hr className="p-0 m-0 opacity-100 border-3 text-rtools-blue-100" />
-      <div className="p-2" />
-      <div className="px-2.5 pb-3">
-        <div className={`rounded-xl bg-[#1e1e1e] w-full p-1`}>
-          <Editor
-            style={{ rounded: "lg" }}
-            height="70vh"
-            defaultLanguage="javascript"
-            defaultValue="// some comment"
-            theme="vs-dark"
-          />
-        </div>
+      <div className={`rounded-xl bg-[#1e1e1e] w-full p-1 mx-2.5 my-3`}>
+        <Editor
+          beforeMount={setEditorTheme}
+          style={{ rounded: "lg" }}
+          height="70vh"
+          language={LANGUAGES[select]}
+          value={codes[select]}
+          theme="my-theme"
+          options={{
+            readOnly: true,
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+          }}
+        />
       </div>
     </div>
   );
