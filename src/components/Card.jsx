a80bcdaa37958a2dca59f6945c925ab984acdfd9
Lackline <victor2965777@gmail.com>
2023-10-18 17:23:37 -0700

    fixed href

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import { BsFillBarChartFill } from "react-icons/bs";
 
 const Card = ({ icon, description, name, color, link }) => {
   return (
-    <Link to={link}>
+    <Link href={link}>
       <div className="hover:-translate-y-1 transition-all duration-300 max-w-xs rounded overflow-hidden shadow-lg">
         <div className="px-3 pt-3 pb-2">
           <Icon color="teal" size="xl" icon={<BsFillBarChartFill />} />
