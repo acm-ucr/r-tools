6ade0663a48ee61f6b903caeb59695b11ff22d47
menthy-wu <wumengxuan0728@gmail.com>
2023-10-19 16:47:40 -0700

    fix props

--- src/components/math/BoolToolBar.jsx
+++ src/components/math/BoolToolBar.jsx
@@ -1,25 +1,7 @@
 "use client";
 import React from "react";
 
-const BoolToolBar = ({ and, or, not, setAnd, setOr, setNot }) => {
-  const symbols = { and: "&", or: "|", not: "~" };
-
-  function setSymbols(symbol, userInput) {
-    switch (symbol) {
-      case "and":
-        setAnd(userInput);
-        break;
-      case "or":
-        setOr(userInput);
-        break;
-      case "not":
-        setNot(userInput);
-        break;
-      default:
-        break;
-    }
-  }
-
+const BoolToolBar = ({ symbols, setSymbols }) => {
   return (
     <div className=" flex-col grid max-w-xs grid-cols-3">
       {Object.entries(symbols).map(([symbol, value], index) => (
@@ -32,9 +14,9 @@ const BoolToolBar = ({ and, or, not, setAnd, setOr, setNot }) => {
             <input
               className="bg-transparent w-8 h-6 rounded outline-none"
               defaultValue={value}
-              value={symbols[index]}
+              value={symbols[symbol]}
               onChange={(e) => {
-                setSymbols(index, e.target.value);
+                setSymbols({ ...symbols, [symbol]: e.target.value });
               }}
             />
           </div>
