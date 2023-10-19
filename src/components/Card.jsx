2aee678ae714b8e01167b18ce54271aef0db6fae
Lackline <victor2965777@gmail.com>
2023-10-18 19:17:17 -0700

    fixed extra div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,16 @@ import { BsFillBarChartFill } from "react-icons/bs";
 
 const Card = ({ icon, description, name, color, link }) => {
   return (
-    <Link href={link}>
-      <div className="hover:-translate-y-1 transition-all duration-300 max-w-xs rounded overflow-hidden shadow-lg">
-        <div className="px-3 pt-3 pb-2">
-          <Icon color="teal" size="xl" icon={<BsFillBarChartFill />} />
-        </div>
-        <div className="px-3">
-          <div className="font-bold text-xl">Selection Sort</div>
-          <p className="text-gray-400 text-base">Visualize selection sort</p>
-        </div>
+    <Link
+      href={link}
+      className="bg-blue-300 hover:-translate-y-1 transition-all duration-300 max-w-xs rounded overflow-hidden shadow-lg"
+    >
+      <div className="px-3 pt-3 pb-2">
+        <Icon color="teal" size="xl" icon={<BsFillBarChartFill />} />
+      </div>
+      <div className="px-3">
+        <div className="font-bold text-xl">Selection Sort</div>
+        <p className="text-gray-400 text-base">Visualize selection sort</p>
       </div>
     </Link>
   );
