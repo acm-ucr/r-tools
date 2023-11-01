878a77f646f07b7c7e55da9e0f25605a3a2f457f
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-11-01 16:29:22 -0700

    Created equation table

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
+"use client"
+import {useState} from "react";
+import Equations from "@/components/math/Equations";
 const Home = () => {
-  return <>home</>;
+
+  const test = ["x|y","x&y&z","x'|y'","x|y'","x&(y'|z)"]
+  const [equations, setEquations] = useState(test);
+  return (
+  
+  <>
+  
+  
+  <Equations equations={equations} setEquations={setEquations}/>
+  
+  
+  </>);
 };
 
 export default Home;
--- src/components/math/Equations.jsx
+++ src/components/math/Equations.jsx
@@ -1,5 +1,41 @@
+"use client";
+import { useState } from "react";
+
 const Equations = ({ equations, setEquations }) => {
-  return <div>Equations</div>;
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  function removeEquation(index) {
+    let newEquations = equations;
+    newEquations.splice(index, 1);
+    setEquations(newEquations);
+  }
+
+  return (
+    <div className="w-64">
+      <div className="flex-row grid bg-blue-300/10 rounded-2xl">
+        <div className="mx-4 divide-y-2 divide-blue-100">
+          {Object.entries(equations).map(([equation, value], index) => (
+            <div
+              key={index}
+              className="grid my-2 justify-between grid-cols-2"
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <div>{value}</div>
+              {hoveredIndex === index && (
+                <div
+                  className="justify-self-end text-slate-400 hover:text-white"
+                  onClick={removeEquation(index)}
+                >
+                  x
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Equations;
