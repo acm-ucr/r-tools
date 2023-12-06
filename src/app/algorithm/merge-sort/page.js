5c6f80d10efa8161af1f935a2f116a938f162350
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-12-05 21:22:15 -0800

    Created mergesort page

--- src/app/algorithm/merge-sort/page.js
+++ src/app/algorithm/merge-sort/page.js
@@ -1,5 +1,18 @@
+"use client";
+import AlgorithmWrapper from "@/components/algorithm/AlgorithmWrapper";
+import { code, sort } from "@/util/sort/mergeSort";
+import { example } from "@/util/sort/mergeSort";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <AlgorithmWrapper
+        code={code}
+        sort={sort}
+        example={example}
+        title="Merge Sort"
+      />
+    </>
+  );
 };
 
 export default page;
