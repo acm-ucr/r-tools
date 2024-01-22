20726d698473be7ca228282056ef1ec078808d53
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2024-01-21 16:17:52 -0800

    created shell sort page

---
+++ src/app/algorithm/shell-sort/page.js
@@ -0,0 +1,20 @@
+"use client";
+import AlgorithmWrapper from "@/components/algorithm/AlgorithmWrapper";
+import { code, sort } from "@/util/sort/shellSort";
+import { example } from "@/util/sort/shellSort";
+
+
+const page = () => {
+  return (
+    <>
+      <AlgorithmWrapper
+        code={code}
+        sort={sort}
+        example={example}
+        title="Shell Sort"
+      />
+    </>
+  );
+}
+
+export default page;
