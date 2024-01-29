a7f2f14a06d1689063a2eecd95376f85cf7b4408
Tokkia <hmiku4329@gmail.com>
2024-01-28 17:39:03 -0800

    test pull req

--- src/data/icon.js
+++ src/data/icon.js
@@ -2,32 +2,32 @@ import { FaPlay, FaPause, FaRandom, FaRedo, FaCode } from "react-icons/fa";
 import { LuStepForward, LuStepBack } from "react-icons/lu";
 
 export const ICONS = [
-    {
-        name: "play",
-        icon: <FaPlay/>,
-    },
-    {
-        name: "pause",
-        icon: <FaPause/>,
-    },
-    {
-        name: "random",
-        icon: <FaRandom/>,
-    },
-    {
-        name: "redo",
-        icon: <FaRedo/>,
-    }, 
-    {
-        name: "code",
-        icon: <FaCode/>,
-    },
-    {
-        name: "stepforward",
-        icon: <LuStepForward/>,
-    }, 
-    {
-        name: "stepback",
-        icon: <LuStepBack/>,
-    },
+  {
+    name: "play",
+    icon: <FaPlay />,
+  },
+  {
+    name: "pause",
+    icon: <FaPause />,
+  },
+  {
+    name: "random",
+    icon: <FaRandom />,
+  },
+  {
+    name: "redo",
+    icon: <FaRedo />,
+  },
+  {
+    name: "code",
+    icon: <FaCode />,
+  },
+  {
+    name: "stepforward",
+    icon: <LuStepForward />,
+  },
+  {
+    name: "stepback",
+    icon: <LuStepBack />,
+  },
 ];
